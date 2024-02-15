228c1fe21da0c830e351f60078bb5ddf57cc0364
Sharafat Karim <60249869+Sharafat78670@users.noreply.github.com>
2024-02-15 09:52:41 +0600

    Added score section

---
+++ play.js
@@ -0,0 +1,7 @@
+function play() {
+    const home =document.getElementById('homePlay');
+    home.classList.add('hidden');
+
+    const playSection=document.getElementById('playground');
+    playSection.classList.remove('hidden');
+}
